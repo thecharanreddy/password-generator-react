6fd21085ead3fd6a48505e6399e954f0748b1ffc
Charan Reddy <charanreddy@Charans-MacBook-Air.local>
2025-01-30 14:06:42 +0530

    first commit

--- password/src/App.jsx
+++ password/src/App.jsx
@@ -1,35 +1,94 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useCallback, useEffect, useState, useRef } from 'react'
+
+
 
 function App() {
-  const [count, setCount] = useState(0)
+
+  const [length, setLength] = useState(6)
+  const [password,setPassword] = useState("")
+  const [includeNums,setIncludeNums] = useState(false)
+  const [includeSymbols,setIncludeSymbols] = useState(false)
+
+  //useRef
+  const passwordRef = useRef(null)
+
+  const PasswordGenerator = useCallback(()=>{
+    let pass=''
+    let str ='abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'
+    if(includeNums) str +='0123456789'
+    if(includeSymbols) str += '!@#$%^&*()_+-=[]{}'
+
+    for(let i = 0; i < length; i++){
+      let char = Math.floor(Math.random()*str.length + 1);
+      pass += str.charAt(char)
+    }
+
+    setPassword(pass)
+  },[length, includeNums, includeSymbols, setPassword])
+  
+  const copyPasswordToClipboard = useCallback(() => {
+    passwordRef.current?.select();
+    passwordRef.current?.setSelectionRange(0, 999);
+    window.navigator.clipboard.writeText(password)
+  }, [password])
+
+
+   useEffect(()=>{
+    PasswordGenerator()
+    },[length, includeNums, includeSymbols, PasswordGenerator])
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    
+      <h1 className=" mt-15 mb-2 mx-5 p-5 text-5xl text-center text-orange-300 " >Password Generator</h1>
+    
+      <div className='w-full max-w-md mx-auto p-4 my-4 bg-gray-700 text-orange-400 shadow-md rounded-lg'>
+       
+        <div className='my-1.5 p-2'>
+          <input type="text" 
+           placeholder='password'
+           value={password} 
+           readOnly
+           className= "bg-gray-500 py-1 px-3 rounded-lg"
+           ref={passwordRef}
+           ></input>
+          <button onClick={copyPasswordToClipboard} className='p-0.5 bg-blue-600 text-white rounded-lg '>Copy</button>
+        </div>
+
+        <div>
+          <input type='range' 
+            min='6'
+            max='20'
+            value = {length}
+            onChange={(e) => {parseInt(setLength(e.target.value))}}
+          ></input>
+          <label>length:{length}</label>
+        </div>
+
+        <div>
+          <input type="checkbox" 
+           id ="includeNums"
+           defaultChecked={includeNums}
+           onChange={()=>{
+            setIncludeNums((prev) => !prev)
+           }}
+          ></input>
+          <label htmlFor='includeNums'>Numbers</label>
+        </div>
+
+        <div>
+          <input type="checkbox"
+            id="includeSymbols"
+            defaultChecked={includeSymbols}
+            onChange={()=>
+              {setIncludeSymbols((prev)=>!prev)} } 
+          ></input>
+          <label htmlFor='includeSymbols'>Symbols</label>
+        </div>
+
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
-  )
+  ) 
 }
 
 export default App
